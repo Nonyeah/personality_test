03cc75cbef3e9d14c1d77c7f564cbc4394893767
nonyeah <nonyeulasi@hotmail.com>
2026-08-02 00:49:36 +0100

    added contact us page

---
+++ client/src/Routes/Contact.tsx
@@ -0,0 +1,14 @@
+function Contact(){
+    return (
+        <div className="contact">
+            <h1>Contact Us </h1>
+            <p>We hope you enjoyed completing our free personality survey and you're happy with your profile results.</p>
+            <p> Although the test survey is based on real-life character models the survey results are meant
+                to be used for enjoyment purposes and should not constitute any form of psychological or psychiatric advice.
+            </p>
+
+            </div>
+    )
+}
+
+export default Contact
