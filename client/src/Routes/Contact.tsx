062cc013663a89bcbba9b4ffb0437726b893d72d
nonyeah <nonyeulasi@hotmail.com>
2026-08-03 20:54:10 +0100

    Minor changes to contact us page

--- client/src/Routes/Contact.tsx
+++ client/src/Routes/Contact.tsx
@@ -6,6 +6,7 @@ function Contact(){
             <p> Although the test survey is based on real-life character models the survey results are meant
                 to be used for enjoyment purposes and should not constitute any form of psychological or psychiatric advice.
             </p>
+            <p>There's no formal way to contact us at present. All the best with your future goals and life endeavours</p>
 
             </div>
     )
