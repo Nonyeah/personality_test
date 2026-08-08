f4b2d6515eccdffe166c7e92f27a086119d3b5d5
nonyeah <nonyeulasi@hotmail.com>
2026-08-08 01:09:26 +0100

    minor format changes

--- client/src/Routes/Error404.tsx
+++ client/src/Routes/Error404.tsx
@@ -3,9 +3,9 @@
     
     <div className="error-route">
         <h1>Hey There!</h1>
-        <p> You must have clicked either the cookies, terms or affiliates page link</p>
-        <p>As this personality assessment is for enjoyment purposes those pages don't exist on this web app</p>
-        <p> Anyhoo...Please use the top navigation links to get yourself back on track</p>
+        <p> You must have clicked either the cookies, terms or affiliates page link
+        As this personality assessment is for enjoyment purposes those pages don't exist on this web app
+        Anyhoo...Please use the top navigation links to get yourself back on track</p>
     </div>
 
     )
